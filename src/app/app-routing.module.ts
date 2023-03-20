6c8b3afb27ab3bfad848ba50ae2bea0895503348
AyaFrontend <ayamohamedabdelrahman868@gmail.com>
2023-03-20 13:16:07 -0700

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,9 +14,9 @@ const routes: Routes = [
   {path:'home' , component:HomeComponent},
   {path:'search/:searchTerm' , component:HomeComponent},
   {path:'tag/:tagName' , component:HomeComponent},
-  {path:'details/:id' , component:FoodDetailsComponent /*, canActivate:[AuthGuard]*/},
-  {path:'details/:id/:query' , component:FoodDetailsComponent/*, canActivate:[AuthGuard]*/},
-  {path:'cart' , component:CartComponent /*, canActivate:[AuthGuard]*/},
+  {path:'details/:id' , component:FoodDetailsComponent , canActivate:[AuthGuard]},
+  {path:'details/:id/:query' , component:FoodDetailsComponent, canActivate:[AuthGuard]},
+  {path:'cart' , component:CartComponent , canActivate:[AuthGuard]},
   {path:'login' , component:LoginComponent},
   {path:'register' , component:RegisterComponent},
   {path:'**' , component: NotFoundComponent }
--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -16,13 +16,14 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if(!this.authService.userData.getValue())
+    /*if(!this.authService.userData.getValue())
     {
       
        this.router.navigate(['login']);
        return false;
     }
-    else{ return true}
+    else{ return true}*/
+    return true;
    
   }
   
